a881e536c7e9dc27e864e9dcc99c3bd80a4e6a32
Gustavo Buratto <gustavo_buratto@hotmail.com>
2024-11-10 14:30:40 +0000

    Implementa o layout de card de favoritos.

--- apps/front/src/app/components/home/home.component.ts
+++ apps/front/src/app/components/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { map } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { FavoritoService } from '../../services/favorito.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    MatGridListModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
     CommonModule,
   ],
   templateUrl: './home.component.html',
@@ -19,16 +21,5 @@ export class HomeComponent {
 
   private favoritoService = inject(FavoritoService)
   public favoritos$ = this.favoritoService.getAll();
-
-  public breakpointObserver = inject(BreakpointObserver);
-
-  public umaColuna$ = this.breakpointObserver.observe([
-    Breakpoints.XSmall,
-    Breakpoints.Small,
-  ]).pipe(map(state => state.matches));
-
-  public duasColuna$ = this.breakpointObserver.observe([
-    Breakpoints.Medium,
-  ]).pipe(map(state => state.matches));
 
 }
